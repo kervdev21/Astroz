a21af2e8c474398aac26b2f756467b97d5d7b4da
kervdev21 <kervdev21@gmail.com>
2021-10-03 11:57:10 -0400

    fixing home

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header() {
   return (
     <div className="relative flex flex-col py-16 lg:pt-0 lg:flex-col lg:pb-0">
-    <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto lg:px-8 lg:max-w-screen-xl">
+    <div className="flex flex-col items-start w-full max-w-xl px-4 mx-auto mt-12 lg:px-8 lg:max-w-screen-xl">
       <div className="mb-16 lg:my-40 lg:max-w-lg lg:pr-5">
         <div className="max-w-xl mb-6">
           <div className="text-center">
@@ -39,9 +39,9 @@ function Header() {
         </div>
       </div>
     </div>
-    <div className="inset-y-0 right-0 w-full max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
+    <div className="inset-y-0 right-0 w-full hidden md:visible lg:visible max-w-xl px-4 mx-auto lg:pl-8 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-1/2 lg:max-w-full lg:absolute xl:px-0">
       <img
-        className="object-cover w-full invisible md:visible lg:visible h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
+        className="object-cover w-full h-56 rounded shadow-lg lg:rounded-none lg:shadow-none sm:h-96 lg:h-full"
         src="https://i.imgur.com/9sGdQFU.png"
         alt=""
       />
